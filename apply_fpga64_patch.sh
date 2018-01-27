bbe5ef1deab303389e67b40b4f232224a9de0787
Emard <vordah@gmail.com>
2018-01-27 22:20:21 +0100

    update apply patch

--- apply_fpga64_patch.sh
+++ apply_fpga64_patch.sh
@@ -14,9 +14,9 @@ cd rtl_renommes
 cat ../fpga64.patch | tr '\\\\' "/" | patch -p1 --binary
 cd ..
 
-cp rtl_renommes/fpga64_buslogic_roms_mmu.vhd              rtl_dar/_fpga64_buslogic_roms_mmu.vhd
-cp rtl_renommes/fpga64_sid_iec.vhd                        rtl_dar/_fpga64_buslogic_sid_iec.vhd
-cp rtl_renommes/fpga64_keyboard_matrix_mark_mcdougall.vhd rtl_dar/_fpga64_keyboard_matrix_mark_mcdougall.vhd
-cp rtl_renommes/video_vicII_656x_a.vhd                    rtl_dar/_video_vicII_656x_a.vhd
+cp rtl_renommes/fpga64_buslogic_roms_mmu.vhd              rtl_dar/fpga64_buslogic_roms_mmu.vhd
+cp rtl_renommes/fpga64_sid_iec.vhd                        rtl_dar/fpga64_buslogic_sid_iec.vhd
+cp rtl_renommes/fpga64_keyboard_matrix_mark_mcdougall.vhd rtl_dar/fpga64_keyboard_matrix_mark_mcdougall.vhd
+cp rtl_renommes/video_vicII_656x_a.vhd                    rtl_dar/video_vicII_656x_a.vhd
 
 rm -rf rtl_renommes

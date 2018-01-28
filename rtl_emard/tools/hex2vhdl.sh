4128033dbd2ce8414ae482e85d2288cfc12e26ed
Emard <vordah@gmail.com>
2018-01-28 20:02:57 +0100

    hex2vhdl conversion tool

---
+++ rtl_emard/tools/hex2vhdl.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+objcopy -I ihex $1 -O binary /tmp/hex2vhdl.bin
+dd bs=512 if=/tmp/hex2vhdl.bin conv=sync \
+| hexdump -v -e '8/1 "x_%02X_, ""\n"' \
+| sed -e 's/_/"/g'
+rm /tmp/hex2vhdl.bin
